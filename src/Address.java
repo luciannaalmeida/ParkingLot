de07aa465cbdffc33723f4d129de6f2da8944342
Lucianna Thomaz Almeida <lucianna.th@gmail.com>
2011-11-02 02:36:20 -0200

    creating classes client and address

---
+++ src/Address.java
@@ -0,0 +1,9 @@
+
+public class Address {
+	private String street;
+	private String number;
+	private String complement;
+	private String neighbourhood;
+	private String cep;
+	private String city;
+}
---
+++ src/Client.java
@@ -0,0 +1,11 @@
+
+public class Client {
+	private String name;
+	private String email;
+	private String phone_number;
+	private String celphone_number;
+	private String cpf;
+	private String birthday;
+	private Address address;
+	
+}
